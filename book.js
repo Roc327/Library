4f36fba106f0a594a1efa150fd89e5e1632dc6f2
Nate <nate_van@hotmail.com>
2024-02-08 12:28:02 -0500

    Added container div's to html and iterator function for library array

--- book.js
+++ book.js
@@ -15,6 +15,12 @@ function addBookToLibrary() {
   // Do stuff here
 }
 
+function displayBooks(arr) {
+  for(let i = 0, i < arr.length - 1, i ++) {
+    // Code to display book to page. Can use a table to display or each can have their own "card" etc.
+  }
+}
+
 let testBook = new Book("American Gods", "Neil Gaiman", 560, "Have read");
 
 myLibrary.push(testBook);
