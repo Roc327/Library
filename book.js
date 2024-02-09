18d992b6f1a6149023449b6fcafe282b3bf0221a
Nate <nate_van@hotmail.com>
2024-02-09 00:17:05 -0500

    added img for header, set up grid and added some styling for basic layout

--- book.js
+++ book.js
@@ -15,11 +15,17 @@ function addBookToLibrary() {
   // Do stuff here
 }
 
+function deleteBooks(arr) {
+  // Code to delete books from array when user presses delete on the displayed card
+}
+
 function displayBooks(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
+  let 
+  //for (let i = 0; i < arr.length - 1; i++) {
     // Code to display book to page. Can use a table to display or each can have their own "card" etc.
   }
 }
+
 
 let testBook = new Book("American Gods", "Neil Gaiman", 560, "Have read");
 
